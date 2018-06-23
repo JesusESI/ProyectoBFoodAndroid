b666fdd33f0103dcb592979224875a42b4429d01
jesus <jesus.alfacode@gmail.com>
2018-06-23 12:03:33 +0200

    Terminada datos usuario Activity.

--- app/src/main/java/jesus/com/proyectobfoodandroid/DatosUsuarioActivity.java
+++ app/src/main/java/jesus/com/proyectobfoodandroid/DatosUsuarioActivity.java
@@ -1,42 +1,43 @@
 package jesus.com.proyectobfoodandroid;
 
+import android.net.Uri;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.google.firebase.database.ChildEventListener;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
-import jesus.com.proyectobfoodandroid.Adapters.DatosUsuarioAdapter;
-import jesus.com.proyectobfoodandroid.Adapters.RankingAdapter;
 import jesus.com.proyectobfoodandroid.Firebase.FirebaseManager;
 import jesus.com.proyectobfoodandroid.Objects.User;
+
+import static android.content.ContentValues.TAG;
 
 public class DatosUsuarioActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-    private RecyclerView recyclerView;
-    private DatosUsuarioAdapter itemUserAdapter;
     private FirebaseDatabase firebase;
+    private FirebaseStorage firebaseStorage;
     private DatabaseReference dbReference;
-    private List<User> usuarioSelected;
 
-    private User userLogDatosUsuario;
+    private User emailDatosUsuario;
+    private Uri imagenUserUri;
+
+    private ImageView imagenUsuario;
+    private TextView apodoTexto, posiciontexto, puntosTexto, logrosTexto, notificacionesTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,62 +49,73 @@ public class DatosUsuarioActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         //Obtenermos el email del usuario logueado.
-        userLogDatosUsuario = new User(getIntent().getStringExtra("email"));
-
-        usuarioSelected = new ArrayList<>();
+        emailDatosUsuario = new User(getIntent().getStringExtra("email"));
 
         // Creamos la query para obtener el usuario.
         firebase = FirebaseManager.getFirebaseSingleton().getmDatabase();
         dbReference = firebase.getReference("Usuarios");
+        firebaseStorage = FirebaseManager.getFirebaseSingleton().getmStorage();
 
-        recyclerView = (RecyclerView) findViewById(R.id.dataUserLayout);
-        recyclerView.setHasFixedSize(true);
+        apodoTexto = findViewById(R.id.textApodo);
+        posiciontexto = findViewById(R.id.textPosicion);
+        puntosTexto = findViewById(R.id.textPuntos);
+        logrosTexto = findViewById(R.id.textLogros);
+        notificacionesTexto = findViewById(R.id.textNotificaciones);
+        imagenUsuario = findViewById(R.id.imageUser);
 
-        LinearLayoutManager linearManager = new LinearLayoutManager(this);
-        linearManager.setOrientation(LinearLayoutManager.VERTICAL);
-
-        recyclerView.setLayoutManager(linearManager);
 
         obtainUser();
-
-        // Creamos adaptador
-        itemUserAdapter = new DatosUsuarioAdapter(usuarioSelected);
-        recyclerView.setAdapter(itemUserAdapter);
     }
 
     public void obtainUser() {
-        dbReference.addChildEventListener(new ChildEventListener() {
+        dbReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                HashMap usuario = (HashMap) dataSnapshot.getValue();
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    Log.d(TAG, data.getValue().toString());
 
-                String email = (String) usuario.get("email");
+                    HashMap usuario = (HashMap) data.getValue();
 
-                if (email.equals(userLogDatosUsuario.getEmail())) {
-                    // TODO. Introducir todos los parametros.
-                    usuarioSelected.add(new User());
-                    itemUserAdapter.notifyDataSetChanged();
+                    if (emailDatosUsuario.getEmail().equals(usuario.get("email"))) {
+
+                        String apodo = (String) usuario.get("apodo");
+                        String puntos = (String) usuario.get("puntos");
+                        String imagen = (String) usuario.get("imagen");
+
+                        // Obtener URI de la imagen del usuario.
+                        obtainUri(imagen);
+
+                        apodoTexto.setText(apodo);
+                        puntosTexto.setText(puntos);
+
+                    }
                 }
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
+    }
+
+    private void obtainUri(String nombreImagen) {
+
+        firebaseStorage.getReference("Imagenes").child(nombreImagen).getDownloadUrl().addOnSuccessListener(
+                new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        // Obtenemos la Uri y actializamos la imagen.
+                        imagenUserUri = uri;
+                        // Libreria externa que carga los datos direcatmente desde la url.
+                        Glide.with(DatosUsuarioActivity.this).load(imagenUserUri).into(imagenUsuario);
+
+                    }
+                }
+        ).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // Tratar el error.
             }
         });
     }
